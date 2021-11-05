2e4c3c9e44af8f6628871c69aed879bca8851032
Luz Olague <olague798317@gmail.com>
2021-11-04 19:44:25 -0700

    avance con kinetic body 2D

---
+++ player-2/red player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+const ACCELERATION = 580
+const MAX_SPEED = 880
+const FRICTION = 200
+
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	
+	var input_vector = Vector2.ZERO
+	input_vector.x= Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_vector.y= Input.get_action_strength("down") - Input.get_action_strength("up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	velocity=move_and_slide(velocity)
