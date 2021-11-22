c36513cc1967a5aef0415d101746f744248acfac
Luz Olague <olague798317@gmail.com>
2021-11-21 19:53:42 -0700

    mejoras en la fisica del juego

--- pelota.gd
+++ pelota.gd
@@ -1,16 +1,12 @@
 extends RigidBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	gravity_scale = 0.0 # remove gravity force!
+	
 
+	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _integrate_forces(delta):
+	set_bounce(0.8)
+
