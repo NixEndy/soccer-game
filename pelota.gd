81b214408743f2cf3ad3010ba7c062d42ccf3433
Luz Olague <olague798317@gmail.com>
2021-11-21 21:46:38 -0700

    mejora de fisicas en el juego

--- pelota.gd
+++ pelota.gd
@@ -1,12 +1,21 @@
 extends RigidBody2D
 
 
+
 func _ready():
 	gravity_scale = 0.0 # remove gravity force!
 	
+	
+func impulse(amount):
+	apply_central_impulse(amount)
+	angular_velocity = amount.lenght/10
 
 	
-
 func _integrate_forces(delta):
-	set_bounce(0.8)
+	set_bounce(1)
+
+
+func _on_pelota_body_entered(body):
+	print(body.name)	
+	$rebote.play()
 
---
+++ porteriaRojo.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var sound_has_played = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_porteriaRojo_body_shape_entered(body_id, body, body_shape, local_shape):
+	print("Gol en porteria de equipo rojo:",body.name)
+	if body.name == "pelota":
+		body.queue_free()
+		$gol.play()
+	
+	
+
+
+
